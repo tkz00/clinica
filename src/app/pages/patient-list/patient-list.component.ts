ef74a9c8cfec4bb95367afbb3679234205d9bb4c
tkz00 <theokatz@gmail.com>
2023-06-28 17:32:45 -0300

    detalle ultimos turnos listado pacientes del especialista

--- src/app/pages/patient-list/patient-list.component.ts
+++ src/app/pages/patient-list/patient-list.component.ts
@@ -19,12 +19,18 @@ export class PatientListComponent {
 
   async ngOnInit(): Promise<void> {
     this.isLoading = true;
-    const result = await this.turnosService.getTurnosByEspecialistaId(this.authService.usuarioDB.id);
-    result.forEach(turn => {
+    const especialistaTurns = await this.turnosService.getTurnosByEspecialistaId(this.authService.usuarioDB.id);
+    console.log(this.users);
+    especialistaTurns.forEach(async turn => {
+      console.log(this.users);
+      const pacienteTurns = await this.turnosService.getTurnosByPacienteId(turn.data.paciente.id);
+      pacienteTurns.sort((a, b) => b.data['fechaHora'].toDate().getTime() - a.data['fechaHora'].toDate().getTime());
       if (!this.users.some(user => user.id === turn.data.paciente.id)) {
+        turn.data.paciente.turns = pacienteTurns.slice(0, 3);
         this.users.push(turn.data.paciente);
       }
     });
+    console.log(this.users);
     this.isLoading = false;
   }
 }
